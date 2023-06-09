8f71e4a875b9a7c8e85f1e6212efcd7e190f20d6
renish47 <ineroff77@gmail.com>
2023-06-09 23:08:57 +0530

    update navBar.js

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,12 +12,15 @@ const NavBar = ({ isLoggedIn, setCloseModal }) => {
     <div className="navBar fixed left-0 top-0 z-10 flex h-[60px] w-full items-center justify-between px-6 text-white sm:px-10">
       <span className=" text-2xl font-semibold">url-sm</span>
       {isLoggedIn ? (
-        <span className=" cursor-pointer text-[15px]" onClick={logoutHandler}>
+        <span
+          className=" cursor-pointer text-[13px] sm:text-[15px]"
+          onClick={logoutHandler}
+        >
           Logout
         </span>
       ) : (
         <span
-          className=" cursor-pointer text-[15px]"
+          className=" cursor-pointer text-[13px] sm:text-[15px]"
           onClick={() => setCloseModal(false)}
         >
           Login
